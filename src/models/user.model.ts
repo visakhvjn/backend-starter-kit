9e82d50b3c1bdc65d5667715c3ae95cec0195d94
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-17 17:53:35 +0530

    adding is email verified

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,12 +4,18 @@ export interface IUser extends Document {
 	email: string;
 	password: string;
 	name: string;
+	isEmailVerified?: boolean;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
 const UserSchema: Schema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	isEmailVerified: { type: Boolean, default: false },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
